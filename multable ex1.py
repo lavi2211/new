065b628e802fe5bf0254ef6ad24a6f1a3bda6bca
lavi2211 <117894758+lavi2211@users.noreply.github.com>
2023-09-12 18:52:25 +0530

    Add files via upload

---
+++ multable ex1.py
@@ -0,0 +1,21 @@
+n=int(input("enter the number for mul table:"))
+print("file was chnaged by developer")
+
+if(n<=0):
+    print("{} is a invalid input:".format(n))
+else:
+    print("mul table for{}".format(n))
+    print("="*50)
+    for i in range(1,11):
+        print("\t{}*{}={}".format(n,i,n*i))
+    else:
+        print("="*60)
+        print("="*16)
+
+
+
+
+
+
+
+
